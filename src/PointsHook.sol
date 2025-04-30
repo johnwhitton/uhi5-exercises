f2860c339e9f614a71df4d5d413586fcb305402a
johnwhitton <john@johnwhitton.com>
2025-04-30 07:23:38 -0700

    Include test and change solc_version to 0.8.26

--- src/PointsHook.sol
+++ src/PointsHook.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.29;
+pragma solidity 0.8.26;
 
 import {BaseHook} from "v4-periphery/src/utils/BaseHook.sol";
 import {ERC20} from "solmate/src/tokens/ERC20.sol";
@@ -14,20 +14,11 @@ import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 import {Hooks} from "v4-core/libraries/Hooks.sol";
 
 contract PointsHook is BaseHook, ERC20 {
-    // Use CurrencyLibrary and BalanceDeltaLibrary
-    // to add some helper functions over the Currency and BalanceDelta
-    // data types
-    // using CurrencyLibrary for Currency;
-    // using BalanceDeltaLibrary for BalanceDelta;
-
-    // Initialize BaseHook and ERC20
     constructor(IPoolManager _manager, string memory _name, string memory _symbol)
         BaseHook(_manager)
         ERC20(_name, _symbol, 18)
     {}
 
-    // Set up hook permissions to return `true`
-    // for the two hook functions we are using
     function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
         return Hooks.Permissions({
             beforeInitialize: false,
@@ -47,7 +38,6 @@ contract PointsHook is BaseHook, ERC20 {
         });
     }
 
-    // Stub implementation of `afterSwap`
     function _afterSwap(
         address,
         PoolKey calldata key,
@@ -55,11 +45,30 @@ contract PointsHook is BaseHook, ERC20 {
         BalanceDelta delta,
         bytes calldata hookData
     ) internal override returns (bytes4, int128) {
-        // We'll add more code here shortly
+        // If this is not an ETH-TOKEN pool with this hook attached, ignore
+        if (!key.currency0.isAddressZero()) return (this.afterSwap.selector, 0);
+
+        // We only mint points if user is buying TOKEN with ETH
+        if (!swapParams.zeroForOne) return (this.afterSwap.selector, 0);
+
+        // Mint points equal to 20% of the amount of ETH they spent
+        // Since its a zeroForOne swap:
+        // if amountSpecified < 0:
+        //      this is an "exact input for output" swap
+        //      amount of ETH they spent is equal to |amountSpecified|
+        // if amountSpecified > 0:
+        //      this is an "exact output for input" swap
+        //      amount of ETH they spent is equal to BalanceDelta.amount0()
+
+        uint256 ethSpendAmount = uint256(int256(-delta.amount0()));
+        uint256 pointsForSwap = ethSpendAmount / 5;
+
+        // Mint the points
+        _assignPoints(hookData, pointsForSwap);
+
         return (this.afterSwap.selector, 0);
     }
 
-    // Stub implementation for `afterAddLiquidity`
     function _afterAddLiquidity(
         address,
         PoolKey calldata key,
@@ -68,7 +77,32 @@ contract PointsHook is BaseHook, ERC20 {
         BalanceDelta,
         bytes calldata hookData
     ) internal override returns (bytes4, BalanceDelta) {
-        // We'll add more code here shortly
+        // If this is not an ETH-TOKEN pool with this hook attached, ignore
+        if (!key.currency0.isAddressZero()) {
+            return (this.afterSwap.selector, delta);
+        }
+
+        // Mint points equivalent to how much ETH they're adding in liquidity
+        uint256 pointsForAddingLiquidity = uint256(int256(-delta.amount0()));
+
+        // Mint the points including any referral points
+        _assignPoints(hookData, pointsForAddingLiquidity);
+
         return (this.afterAddLiquidity.selector, delta);
+    }
+
+    function _assignPoints(bytes calldata hookData, uint256 points) internal {
+        // If no hookData is passed in, no points will be assigned to anyone
+        if (hookData.length == 0) return;
+
+        // Extract user address from hookData
+        address user = abi.decode(hookData, (address));
+
+        // If there is hookData but not in the format we're expecting and user address is zero
+        // nobody gets any points
+        if (user == address(0)) return;
+
+        // Mint points to the user
+        _mint(user, points);
     }
 }
